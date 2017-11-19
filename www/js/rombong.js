cbc0419e1c489875b074d6330a2fa7f13930608a
awalsadja <awalsadja@gmail.com>
2017-11-19 18:03:53 +0700

    3. Add Form Donasi

--- www/js/rombong.js
+++ www/js/rombong.js
@@ -1,14 +1,3 @@
-// Initialize your app
-var myApp = new Framework7();
-
-// Export selectors engine
-var $$ = Dom7;
-
-// Add view
-var mainView = myApp.addView('.view-main', {
-    // Because we use fixed-through navbar we can enable dynamic navbar
-    dynamicNavbar: true
-});
 // Init App
 var myApp = new Framework7({
     modalTitle: 'Rombong Sedekah',
@@ -21,4 +10,34 @@ var $$ = Dom7;
 
 // Add main view
 var mainView = myApp.addView('.view-main', {
+});
+
+/* ===== Pull To Refresh Demo ===== */
+myApp.onPageInit('pull-to-refresh', function (page) {
+    // Dummy Content
+    var songs = ['Yellow Submarine', 'Don\'t Stop Me Now', 'Billie Jean', 'Californication'];
+    var authors = ['Beatles', 'Queen', 'Michael Jackson', 'Red Hot Chili Peppers'];
+    // Pull to refresh content
+    var ptrContent = $$(page.container).find('.pull-to-refresh-content');
+    // Add 'refresh' listener on it
+    ptrContent.on('refresh', function (e) {
+        // Emulate 2s loading
+        setTimeout(function () {
+            var picURL = 'http://lorempixel.com/88/88/abstract/' + Math.round(Math.random() * 10);
+            var song = songs[Math.floor(Math.random() * songs.length)];
+            var author = authors[Math.floor(Math.random() * authors.length)];
+            var linkHTML = '<li class="item-content">' +
+                                '<div class="item-media"><img src="' + picURL + '" width="44"/></div>' +
+                                '<div class="item-inner">' +
+                                    '<div class="item-title-row">' +
+                                        '<div class="item-title">' + song + '</div>' +
+                                    '</div>' +
+                                    '<div class="item-subtitle">' + author + '</div>' +
+                                '</div>' +
+                            '</li>';
+            ptrContent.find('ul').prepend(linkHTML);
+            // When loading done, we need to "close" it
+            myApp.pullToRefreshDone();
+        }, 2000);
+    });
 });
